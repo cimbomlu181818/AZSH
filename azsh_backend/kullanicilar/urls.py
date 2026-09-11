f7c610700e6c036585cee6b467346eba480194fb
harun <cimbomlu181818@gmail.com>
2026-09-11 10:16:49 +0300

    part 16

--- azsh_backend/kullanicilar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('kayit/', views.kayit_ol, name='kayit_ol'),
-    path('dogrula/', views.dogrula, name='dogrula'),
-    path('giris/', views.giris_yap, name='giris_yap'),
-    path('erisim/', views.erisim_kontrol, name='erisim_kontrol'),
-]

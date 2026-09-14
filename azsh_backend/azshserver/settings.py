f94be991325e69000b7d06e7cbe648090f94dcbc
cimbomlu181818 <cimbomlu181818@gmail.com>
2026-09-14 13:04:21 +0300

    part 26

--- azsh_backend/azshserver/settings.py
+++ azsh_backend/azshserver/settings.py
@@ -74,12 +74,25 @@ WSGI_APPLICATION = 'azshserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('NEON_DB_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'HOST': os.environ.get('NEON_DB_HOST'),
+            'NAME': os.environ.get('NEON_DB_NAME'),
+            'USER': os.environ.get('NEON_DB_USER'),
+            'PASSWORD': os.environ.get('NEON_DB_PASSWORD'),
+            'PORT': '5432',
+            'OPTIONS': {'sslmode': 'require'},
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

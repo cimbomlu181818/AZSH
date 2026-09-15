7d97a7c6172e054e084109c4757ea3cb59d649c1
cimbomlu181818 <cimbomlu181818@gmail.com>
2026-09-15 20:55:10 +0300

    part 39

--- azsh_backend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py createsuperuser --noinput || true
-exec gunicorn --bind 0.0.0.0:8080 azshserver.wsgi:application
